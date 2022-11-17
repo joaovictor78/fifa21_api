17108eed2632d445394d5925d1e7daba52bcad4b
João Victor <joaovictorb.dasilva2003@gmail.com>
2022-11-17 12:13:26 -0400

    feat: add database configs

---
+++ src/infra/db/database.config.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm';
+
+export const typeOrmConfig = new DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '123456',
+    database: 'postgres',
+    migrations: [__dirname + '/migration/*{.ts,.js}'],
+    entities: [__dirname + '/../**/*.model{.ts,.js}'],
+    synchronize: true,
+});
