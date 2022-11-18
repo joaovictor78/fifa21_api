cd90d1004e22892729c597e0b34be038a6730d07
João Victor <joaovictorb.dasilva2003@gmail.com>
2022-11-18 11:41:39 -0400

    refactor: change the league name to be unique in the table

--- src/infra/models/league.model.ts
+++ src/infra/models/league.model.ts
@@ -1,4 +1,4 @@
-import PlayersController from '@/web-controllers/players-controller';
+import LeagueEntity from '../../domain/entities/league.entity';
 import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import PlayerModel from './player.model';
 
@@ -6,8 +6,18 @@ import PlayerModel from './player.model';
 export default class LeagueModel {
     @PrimaryGeneratedColumn()
     league_id: number;
-    @Column()
+    @Column({ unique: true })
     name: string;
     @OneToMany(type => PlayerModel, league => LeagueModel)
     players: PlayerModel[];
+
+    static convertToModel(league: string): LeagueModel {
+        const league_model = new LeagueModel();
+        league_model.name = league;
+        return league_model;
+    }
+    convertToEntity(): LeagueEntity {
+        const league_entity = new LeagueEntity(this);
+        return league_entity;
+    }
 }
