bebbebd4df2fac22dd301c66399beb33a38905ef
Jose Vitor <dogb6205@gmail.com>
2022-11-22 14:31:20 -0300

    feat: rewrite file server

--- src/infra/db/database.config.ts
+++ src/infra/db/database.config.ts
@@ -2,7 +2,7 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    url: "postgres://aljwwvhk:ZWc91SZOXM2CfKNL3TinLltUrte0RfJT@babar.db.elephantsql.com/aljwwvhk",
+    url: process.env.DB_URI || "postgres://aljwwvhk:ZWc91SZOXM2CfKNL3TinLltUrte0RfJT@babar.db.elephantsql.com/aljwwvhk",
     migrations: [__dirname + '/migration/*{.ts,.js}'],
     entities: [__dirname + '/../**/*.model{.ts,.js}'],
     synchronize: true,
