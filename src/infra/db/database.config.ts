17143f6bc83bd3bd2b959dbb0acd3f9b427d27e5
Jose Vitor <dogb6205@gmail.com>
2022-11-22 13:25:04 -0300

    feat: rewrite file config db

--- src/infra/db/database.config.ts
+++ src/infra/db/database.config.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
+    host: 'babar.db.elephantsql.com',
     port: 5432,
-    username: 'postgres',
-    password: '1719',
-    database: 'postgres',
+    username: 'aljwwvhk',
+    password: 'ZWc91SZOXM2CfKNL3TinLltUrte0RfJT',
+    database: 'aljwwvhk',
     migrations: [__dirname + '/migration/*{.ts,.js}'],
     entities: [__dirname + '/../**/*.model{.ts,.js}'],
     synchronize: true,
