9ecc7529353c72af5cdc0c9d49a84b856cff38fd
Jose Vitor <dogb6205@gmail.com>
2022-11-23 09:52:48 -0300

    feat: modify file config database

--- src/infra/db/database.config.ts
+++ src/infra/db/database.config.ts
@@ -2,9 +2,12 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    url: process.env.DB_URI || "postgres://aljwwvhk:ZWc91SZOXM2CfKNL3TinLltUrte0RfJT@babar.db.elephantsql.com/aljwwvhk",
+    url: "postgres://aljwwvhk:ZWc91SZOXM2CfKNL3TinLltUrte0RfJT@babar.db.elephantsql.com/aljwwvhk",
     migrations: [__dirname + '/migration/*{.ts,.js}'],
     entities: [__dirname + '/../**/*.model{.ts,.js}'],
     synchronize: true,
-    migrationsRun: true
+    migrationsRun: true,
+    extra: {
+        max: 1
+    }
 });
